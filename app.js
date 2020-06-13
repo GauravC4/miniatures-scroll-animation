174c2fd168a7ce76b8c6a656f83b166a774f9102
Gaurav Chemburkar <gaurav.mrc@gmail.com>
2020-06-14 02:00:00 +0530

    flight path adjusted

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ gsap.registerPlugin(MotionPathPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
 var flightPath = {
+  // path: [
+  //   { x: 100, y: -30 },
+  //   { x: 300, y: 10 },
+  //   { x: 500, y: 100 },
+  //   { x: 750, y: -200 },
+  //   { x: 600, y: -300 },
+  //   { x: 500, y: -200 },
+  //   { x: 500, y: -100 },
+  //   { x: 600, y: 0 },
+  //   { x: 700, y: -50 },
+  //   { x: 800, y: -100 },
+  //   { x: window.innerWidth, y: -300 },
+  // ],
   path: [
-    { x: 100, y: -30 },
-    { x: 300, y: 10 },
-    { x: 500, y: 100 },
-    { x: 750, y: -200 },
-    { x: 600, y: -300 },
-    { x: 500, y: -200 },
-    { x: 500, y: -100 },
-    { x: 600, y: 0 },
-    { x: 700, y: -50 },
-    { x: 800, y: -100 },
+    { x: 100, y: 0 },
+    { x: 200, y: 50 },
+    { x: 600, y: 200 },
+    { x: 800, y: 0 },
+    { x: 600, y: -200 },
+    { x: 200, y: 0 },
+    { x: 600, y: 200 },
+    { x: 800, y: 0 },
     { x: window.innerWidth, y: -300 },
   ],
-  curviness: 0.5,
+  curviness: 0.6,
   autoRotate: true,
 };
 
